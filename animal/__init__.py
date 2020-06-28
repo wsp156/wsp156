31d549c49ba2209eea7a0f0df2010275b1b926ea
Wooseong Park <wsp156@gmail.com>
2020-06-28 09:07:05 +0000

    Initial commit

---
+++ animal/__init__.py
@@ -0,0 +1,2 @@
+from .cat import Cat #. 이 의미하는건, "이 폴더에 있는(현재 폴더)" 파일에서 Cat이라는 클래스를 가져와줘
+from .dog import Dog
---
+++ main.py
@@ -0,0 +1,9 @@
+#from animal import dog #animal 패키디에서 dog라는 패키지를 가져와줘
+#from animal import cat
+
+from animal import *
+
+d = dog.Dog()
+d.hi()
+c = cat.Cat()
+c.hi()
